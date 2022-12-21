7a679dc51f6a3daf23d82798da7265b25f8dad06
Gilded <knukles30@gmail.com>
2022-12-20 22:35:34 -0700

    creation of functions, still not wired to html

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(firstOperand, secondOperand) {
+  return firstOperand + secondOperand;
+}
+
+function substract(firstOperand, secondOperand) {
+  return firstOperand - secondOperand;
+}
+
+function multiply(firstOperand, secondOperand) {
+  return firstOperand * secondOperand;
+}
+
+function divide(firstOperand, secondOperand) {
+  if (firstOperand === 0 || secondOperand === 0) {
+    return "ERROR";
+  }
+  return firstOperand / secondOperand;
+}
+
+function operate(operator, firstOperand, secondOperand) {
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+    case "-":
+      return substract(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
+    case "/":
+      return divide(firstOperand, secondOperand);
+  }
+}

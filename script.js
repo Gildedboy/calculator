f2fc6c326491c00c5e253917cc75c5ff0bd3defa
Gildedboy <knukles30@gmail.com>
2023-04-14 15:03:37 -0700

    Calculator Working BUT its not enough

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function multiply(firstOperand, secondOperand) {
 }
 
 function divide(firstOperand, secondOperand) {
-  if (firstOperand === 0 || secondOperand === 0) {
+  if (firstOperand == 0 || secondOperand == 0) {
     return "ERROR";
   }
   return firstOperand / secondOperand;
@@ -33,12 +33,57 @@ function operate(operator, firstOperand, secondOperand) {
 const display = document.getElementById("display");
 const numberButtons = Array.from(document.getElementsByClassName("number"));
 const clear = document.getElementById("clear");
-
-//console.log(numberButtons);
-
-numberButtons.forEach((button) =>
-  button.addEventListener("click", function () {
-    //console.log(typeof +display.textContent);
-    display.textContent = button.textContent;
-  })
+const operationButtons = Array.from(
+  document.getElementsByClassName("operation")
 );
+const result = document.getElementById("result");
+
+//Clear function
+clear.addEventListener("click", () => {
+  firstOperand = 0;
+  secondOperand = 0;
+  display.textContent = "0";
+});
+
+//Adding Numbers to display Function
+numberButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (display.textContent == 0) {
+      display.textContent = button.textContent;
+    } else {
+      display.textContent += button.textContent;
+    }
+  });
+});
+
+// //Adding Numbers to Operands
+
+// // It looks like I will have to make the text content from string into numbers first in order to make them usable for the operations
+// // First I have to to make a foreach click event listener that will apply to each button... when that occurs.. it will store the number of displaytext content into firstOperand and make the display text content 0
+
+operationButtons.forEach((operationButton) => {
+  firstOperand = 0;
+  secondOperand = 0;
+  operationButton.addEventListener("click", () => {
+    if (display.textContent != 0 && firstOperand === 0) {
+      operationSymbol = operationButton.textContent;
+      firstOperand = parseFloat(display.textContent);
+      display.textContent = 0;
+      console.log(operationSymbol, firstOperand);
+    } else {
+      operationSymbol = operationButton.textContent;
+      secondOperand = parseFloat(display.textContent);
+      display.textContent = 0;
+      console.log(secondOperand);
+    }
+    return [operationSymbol, firstOperand];
+  });
+});
+
+result.addEventListener("click", () => {
+  if (secondOperand == 0) {
+    secondOperand = parseFloat(display.textContent);
+  }
+  operationResult = operate(operationSymbol, firstOperand, secondOperand);
+  display.textContent = operationResult;
+});
